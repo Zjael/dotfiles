bea3ed57827a7aaab191824ccd940a7f0b505120
Jakob Sjaelland <Zjael@users.noreply.github.com>
2017-07-09 17:40:26 +0200

    polybar update

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -2,10 +2,14 @@
 
 # Terminate already running bar instances
 killall -q polybar
+
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar1
-polybar top &
+# Launch bar1 and bar2
+polybar mBar -r &
+if [ $(xrandr -q | grep " connected" | wc -l ) -gt 1 ]; then
+    polybar eBar -r &
+fi
 
 echo "Polybar launched..."
