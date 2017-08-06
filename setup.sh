6c4c750e586d09c73b081812ecc2eb6f2985389b
Jakob Sjaelland <jakob.sjaelland@gmail.com>
2017-08-06 17:33:37 +0200

    Moved install_pacaur to separate file

--- setup.sh
+++ setup.sh
@@ -17,9 +17,6 @@ arch_setup() {
     echo "#-- Setting Keymap --#"
     set_keymap
 
-    echo "#-- Installing Pacaur --#"
-    install_pacaur
-
     echo "#-- Setting Up Firewall --#"
     set_firewall
 
@@ -40,8 +37,10 @@ arch_setup() {
 }
 
 set_locale() {
-    echo "LANG=$LANG.UTF-8" >/etc/locale.conf
-    sed -i "/#$LANG/s/^#//" /etc/locale.gen
+    echo LANG=$LANG.UTF-8 >> /etc/locale.conf
+    echo LANGUAGE=$LANG >> /etc/locale.conf
+    echo "$LANG.UTF-8 UTF-8" >> /etc/locale.gen
+
     locale-gen
 }
 
@@ -54,46 +53,21 @@ set_keymap() {
     echo "KEYMAP=$KEYMAP" >> /etc/vconsole.conf
 }
 
-install_pacaur() {
-    sudo pacman -Syu
-
-    mkdir -p /tmp/pacaur_install
-    cd /tmp/pacaur_install
-
-    sudo pacman -S binutils make gcc fakeroot pkg-config --noconfirm --needed
-    sudo pacman -S expac yajl git --noconfirm --needed
-
-    # Install "cower" from AUR
-    if [ ! -n "$(pacman -Qs cower)" ]; then
-        curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=cower
-        makepkg PKGBUILD --skippgpcheck --install --needed
-    fi
-
-    # Install "pacaur" from AUR
-    if [ ! -n "$(pacman -Qs pacaur)" ]; then
-        curl -o PKGBUILD https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=pacaur
-        makepkg PKGBUILD --install --needed
-    fi
-
-    cd ~
-    rm -r /tmp/pacaur_install
-}
-
 set_firewall() {
     sudo pacman -S ufw --noconfirm --needed
 
-    sudo iptables -F; iptables -X
-    sudo echo 'y' | ufw reset
-    sudo echo 'y' | ufw enable
-    sudo ufw default deny incoming
-    sudo ufw default deny forward
+    iptables -F; iptables -X
+    echo 'y' | ufw reset
+    echo 'y' | ufw enable
+    ufw default deny incoming
+    ufw default deny forward
 
     # ufw logging on
-    sudo ufw allow 22,80,443/tcp
+    ufw allow 22,80,443/tcp
     # ufw allow $ssh_port/tcp
     # ufw allow from 192.168.10.0/24
 
-    sudo systemctl enable --now ufw.service
+    systemctl enable --now ufw.service
 }
 
 set_ssh() {
@@ -119,9 +93,11 @@ set_mirrorlist() {
 
 install_packages() {
     local packages=''
+    # Terminal
+    packages+=' zsh tmux tree ranger autojump thefuck micro'
 
     # General utilities/libraries
-    packages+=' zsh tmux xclip curl httpie htop nethogs tree ranger autojump udevil thefuck micro rar unrar lm_sensors scrot neofetch'
+    packages+=' xclip curl httpie htop nethogs udevil rar unrar lm_sensors scrot neofetch'
 
     # Development
     packages+=' git gist'
@@ -135,12 +111,15 @@ install_packages() {
     # Storage & Data
     packages+=' fzf rsync'
 
+    # Xorg
+    packages+=' xbacklight xorg xorg-xinit'
+
     # Desktop
     packages+=' waterfox-bin spotify blockify emacs'
 
     # Enviroment
     packages+=' i3-gaps i3lock-fancy-git polybar'
-    packages+=' termite rofi feh conky compton dunst xbacklight xorg xorg-xinit rxvt-unicode rxvt-unicode-terminfo'
+    packages+=' termite rofi feh conky compton dunst  rxvt-unicode rxvt-unicode-terminfo'
 
     # Themes
     packages+=' gtk-arc-flatabulous-theme-git paper-icon-theme-git siji-git'
@@ -152,7 +131,7 @@ install_packages() {
     packages+=' xf86-input-synaptics tlp thermald'
 
 
-    pacaur -Sy --needed --noconfirm --noedit $packages
+    pacaur -S --needed --noconfirm --noedit $packages
 }
 
 set_shell() {
