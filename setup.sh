855b34043ac0bb921f3c48a9125b3df3aaf5e4a3
Jakob Sjaelland <jakob.sjaelland@gmail.com>
2017-08-11 14:41:59 +0200

    packages installer to separate file

--- setup.sh
+++ setup.sh
@@ -44,6 +44,9 @@ arch_setup() {
     echo "#-- Setting Up Fstrim --#"
     #setup_fstrim   # Weekly SSD maintenance, make sure your SSD supports TRIM if unsure run 'lsblk -D'
 
+    echo "#-- Installing Pacaur --#"
+    install_pacaur
+
     echo "#-- Installing Packages --#"
     install_packages
 
@@ -122,49 +125,14 @@ setup_fstrim() {
     systemctl enable --now fstrim fstrim.timer
 }
 
+install_pacaur() {
+    chmod +x install_pacaur.sh
+    ./install_pacaur.sh
+}
+
 install_packages() {
-    local packages=''
-    # System utilities
-    packages+=' networkmanager lm_sensors thermald redshift curl wget httpie htop nethogs udevil rar unrar scrot neofetch'
-
-    # Terminal
-    packages+=' zplug tmux tree ranger autojump thefuck micro'
-    packages+=' rtv'
-
-    # Development
-    packages+=' git gist tig'
-    packages+=' python python-pip python-setuptools python-virtualenv python2 python2-pip python2-setuptools python2-virtualenv python-virtualenvwrapper'
-    packages+=' nodejs npm'
-
-    # Security
-    packages+=' lynis firejail'
-
-    # Storage & Data
-    packages+=' fzf rsync'
-
-    # Xorg
-    packages+=' xorg xorg-xinit xbacklight'
-
-    # Desktop
-    packages+=' waterfox-bin spotify blockify emacs xclip cups'
-
-    # For fun
-    packages+=' cowsay lolcat fortune-mod'
-
-    # Enviroment
-    packages+=' i3-gaps i3lock-fancy-git polybar'
-    packages+=' stow termite rofi feh conky compton dunst rxvt-unicode rxvt-unicode-terminfo'
-
-    # Themes
-    packages+=' gtk-arc-flatabulous-theme-git paper-icon-theme-git siji-git'
-
-    # Fonts
-    packages+=' powerline-fonts powerline nerd-fonts-complete'
-
-    #pacaur -S --needed --noconfirm --noedit $packages
-    for pkg in $packages; do
-        pacaur -S --needed --noconfirm --noedit $pkg
-    done
+    chmod +x install_packages.sh
+    ./install_packages.sh
 }
 
 setup_services() {
