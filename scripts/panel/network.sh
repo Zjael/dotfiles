e7c29aaae8e68706b707c7dc99289f0c167708ae
Jakob Sjaelland <jakob.sjaelland@gmail.com>
2017-08-22 19:13:25 +0200

    spacing

--- scripts/panel/network.sh
+++ scripts/panel/network.sh
@@ -4,7 +4,7 @@ current=$(nmcli device wifi 2>/dev/null | tail -n +2 | grep -P "^\*")
 
 # no connection
 if [ -z "$current" ]; then
-  echo "%{F#4dffffff}%{O-2}"
+  echo "%{F#4dffffff}  "
   exit 0
 fi
 
