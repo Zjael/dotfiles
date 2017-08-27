7e96d42b9df78001e564c956cb7a15d732eb46aa
Jakob Sjaelland <jakob.sjaelland@gmail.com>
2017-08-27 13:22:45 +0200

    DPI Update

--- stow.sh
+++ stow.sh
@@ -5,7 +5,7 @@ SILENT=${SILENT-true}
 DIRNAME=$(dirname $0)
 cd $DIRNAME
 
-MODULES=( "compton" "dunst" "i3" "polybar" "rofi" "Xresources" "zsh")
+MODULES=( "compton" "dunst" "i3" "polybar" "rofi" "Xresources" "zsh" "xorg")
 
 ARG="-t /home/$USER"
 
