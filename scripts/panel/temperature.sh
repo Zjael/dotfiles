1d9c7c14705e5b672cfdd4cfae78ce8936013823
Jakob Sjaelland <jakob.sjaelland@gmail.com>
2017-08-29 21:19:07 +0200

    Fixed script

--- scripts/panel/temperature.sh
+++ scripts/panel/temperature.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
+# fork from Per-core temperatures :
+# https://github.com/jaagr/polybar/wiki/User-contributed-modules#per-core-temperatures
+
 # Get information from cores temp thanks to sensors
 rawData=$( sensors | grep Core | awk '{print substr($3, 2, length($3)-5)}' )
 tempCore=($rawData)
 
 # Define constants :
 degree="°C"
-temperaturesValues=(30 40 55 70 90 110)
+temperaturesValues=(30 40 55 80 100 200)
 temperaturesColors=("#6bff49" "#f4cb24" "#ff8819" "#ff3205" "#f40202" "#ef02db")
-temperaturesIcon=""
-
- 1
- 2
- 3
- 4
- 5
+temperaturesIcons=(     )
 
 for iCore in ${!tempCore[*]}
 do
@@ -34,7 +31,9 @@ sum=$(( $total/${#tempCore[*]} ))
 for iTemp in ${!temperaturesValues[*]}
 do
     if (( "$sum" < "${temperaturesValues[$iTemp]}" )); then
-        tmpEcho="%{F${temperaturesColors[$iTemp]}}${temperaturesIcon}"
+        ## Decide if you want the icon colored too, default isn't
+        tmpEcho="%{F${temperaturesColors[$iTemp]}}${temperaturesIcons[$iTemp]}"
+        #tmpEcho="${temperaturesIcons[$iTemp]}"
         finalEcho="$tmpEcho $finalEcho"
         break
     fi
